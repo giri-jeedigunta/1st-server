e9ddab2715d1a66214bffd85d49b59e4455aab16
Giri Jeedigunta <giri.jeedigunta@gmail.com>
2024-05-14 11:32:12 +0200

    Update n2.js

--- n2.js
+++ n2.js
@@ -12,5 +12,5 @@ server.listen(port, (err) => {
   if (err) {
     return console.log('something bad happened - N2', err)
   }
-  console.log(`server is listening on ${port}`)
+  console.log(`server is listening on ${port} N2`)
 })
